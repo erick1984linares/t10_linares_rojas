5c63a608dcc8a33b3a26ea50ca5a0d2edafbe6f4
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2020-01-04 17:30:16 -0500

    se ha terminado la app10

---
+++ linares/app10.py
@@ -0,0 +1,37 @@
+import libreria
+def agregarTrabajador():
+    # 1. Pedir nombre del Trabajador
+    # 2. Pedir la edad del Trabajador (1-100)
+    # 3. Guardar los datos en el archivo info.txt
+    nombre = libreria.pedir_nombre("Ingrese Nombre:")
+    edad = libreria.pedir_numero("Ingrese Edad:", 18, 65)
+    contenido=nombre + "-" + str(edad) + "\n"
+    libreria.guardar_datos("info.txt", contenido, "a")
+    print("Datos del trabajador guardados")
+
+def leerTrabajador():
+    datos = libreria.obtener_datos("info.txt")
+    if ( datos != ""):
+        print(datos)
+    else:
+        print("Archivo sin datos")
+
+opc=0
+max=3
+while(opc != max):
+    print("######## MENU #######")
+    print("1. Agregar Trabajador")
+    print("2. Leer Trabajador")
+    print("3. Salir")
+    print("#####################")
+
+    opc=libreria.pedir_numero("Ingresar Opcion:", 1, 3)
+
+    if (opc == 1):
+        agregarTrabajador()
+    if (opc == 2):
+        leerTrabajador()
+
+#fin_menu
+print("Fin del programa")
+
