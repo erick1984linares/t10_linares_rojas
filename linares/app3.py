fa10f960723a242cf6b4c586f1db76486815ab30
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2020-01-04 17:30:04 -0500

    se ha terminado la app3

---
+++ linares/app3.py
@@ -0,0 +1,37 @@
+import libreria
+
+def agregarUniversidad():
+    universidad=libreria.pedir_universidad("ingrese universidad:")
+    ciclo=libreria.pedir_numero("ingrese ciclo:", 1, 10)
+    nombre_de_la_region=libreria.pedir_region("ingrese nombre de la region:")
+    contenido=universidad + "-" + str(ciclo) + "-" + str(nombre_de_la_region) + "\n"
+    libreria.guardar_datos("info.txt", contenido, "a")
+    print("Datos guardados")
+
+def verDatos():
+    datos = libreria.obtener_datos("info.txt")
+    if ( datos != ""):
+        print(datos)
+    else:
+        print("Archivo sin datos")
+
+opc=0
+max=3
+while(opc != max):
+    print("######## MENU #######")
+    print("1. agregar Uiversidad")
+    print("2. ver datos")
+    print("3. Salir")
+    print("#####################")
+
+    opc=libreria.pedir_numero("Ingresar Opcion:", 1, 3)
+
+    if (opc == 1):
+        agregarUniversidad()
+
+    if (opc == 2):
+        verDatos()
+
+
+#fin_menu
+print("Fin del programa")
