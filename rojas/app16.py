0c7d36ca8e3ee4d853cdfb46c1cc27cbcb2b57d9
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2020-01-10 06:21:15 -0500

    Mi compáñero a terminado su app16

---
+++ rojas/app16.py
@@ -0,0 +1,22 @@
+from Rojas import libreria
+
+
+oppcn=0
+limit=3
+while (oppcn != limit):
+    print("########################")
+    print("#         MENU         #")
+    print("########################")
+    print("# 1. Agregar anotacion #")
+    print("# 2. Ver agregados     #")
+    print("# 3. Salir             #")
+    print("########################")
+
+    oppcn=libreria.pedir_numero("Ingrese la opcion deseada: ",1,3)
+    if (oppcn == 1):
+
+
+    if (oppcn == 2):
+
+
+print("fin del programa")
